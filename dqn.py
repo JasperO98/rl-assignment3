dc9125198bd7daf35a560f4d91b3bc6737f165b3
Wilco de Boer <deboer.wilco@gmail.com>
2020-04-14 23:11:47 +0200

    input shape

--- dqn.py
+++ dqn.py
@@ -13,7 +13,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 class DQN:
     def build_model(self):
         model = Sequential()
-        model.add(Dense(64, input_dim=np.product(self.env.observation_space.shape), activation='relu'))
+        model.add(Dense(64, input_shape=self.env.observation_space.shape, activation='relu'))
         model.add(Dense(32, activation='relu'))
         model.add(Dense(self.env.action_space.n, activation='linear'))
         model.compile(optimizer=Adam(), loss='mse')
