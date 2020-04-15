b33b27002f713f7f03fde7b94c34552c89e7afa8
Wilco de Boer <deboer.wilco@gmail.com>
2020-04-15 16:01:18 +0200

    Start on task 3

---
+++ task3.py
@@ -0,0 +1,49 @@
+from dqn import DQN
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.optimizers import Adam
+
+
+def build_model(input_shape, action_space):
+    model = Sequential()
+    model.add(Conv2D(
+        filters=32,
+        kernel_size=8,
+        strides=(4, 4),
+        padding='valid',
+        activation='relu',
+        input_shape=input_shape,
+        data_format='channels_first',
+    ))
+    model.add(Conv2D(
+        filters=64,
+        kernel_size=4,
+        strides=(2, 2),
+        padding='valid',
+        activation='relu',
+        input_shape=input_shape,
+        data_format='channels_first',
+    ))
+    model.add(Conv2D(
+        filters=64,
+        kernel_size=3,
+        strides=(1, 1),
+        padding='valid',
+        activation='relu',
+        input_shape=input_shape,
+        data_format='channels_first',
+    ))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(action_space))
+    model.compile(optimizer=Adam(), loss='mse')
+    return model
+
+
+def policy(state_c, action, reward, state_n, done):
+    return reward
+
+
+if __name__ == '__main__':
+    dqn = DQN('Breakout-v0', policy, build_model)
+    dqn.train()
