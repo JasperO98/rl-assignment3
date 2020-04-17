f93094c68997cdc6f259ecab960618df9a857e74
J.Ouwerkerk <jasper.ouwerkerk98@gmail.com>
2020-04-17 11:05:42 +0200

    add play_game & tweek plot_json

--- plot_json.py
+++ plot_json.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from os.path import join
 
 
-with open("output/task3_reward.json") as f:
+with open(join('output', 'task3_reward.json')) as f:
     reward = json.load(f)
 
 
@@ -12,8 +12,11 @@ sns.lineplot(x=range(len(reward)), y=[sum(x) for x in reward])
 plt.savefig(join('output', 'task3_reward.pdf'))
 plt.show()
 
-with open("output/task3_loss.json") as f:
+
+print('Loading loss file, this may take a while...')
+with open(join('output', 'task3_loss.json')) as f:
     loss = json.load(f)
+
 
 sns.lineplot(x=range(len(loss)), y=loss)
 plt.savefig(join('output', 'task3_loss.pdf'))
