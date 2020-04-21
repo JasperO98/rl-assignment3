b7e9b33a1c4e200a5c36aabc4e6ef0e30304452b
Wilco de Boer <deboer.wilco@gmail.com>
2020-04-21 18:43:53 +0200

    Fix imports

--- task2.py
+++ task2.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 from keras.optimizers import Adam
 from os.path import join
 
