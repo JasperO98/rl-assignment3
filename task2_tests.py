b86c3c366dc22b4d807977681bfcdc934ea05366
Vincent <vincent1997uithoorn@hotmail.com>
2020-04-28 23:53:37 +0200

    script for 4 tegelijk

--- task2_tests.py
+++ task2_tests.py
@@ -11,6 +11,7 @@ import itertools
 from task2 import SettingsTask2
 import os.path
 
+
 class SettingsHere(SettingsTask2):
     def __init__(self, budget, gamma, alpha, weight_update_interval):
         self.budget = budget
@@ -27,7 +28,6 @@ def test_model(file, games_n, model_name_n):
     model = load_model(file)
     all_rewards = []
     env = make('MountainCar-v0')
-
     for _ in range(games_n):
         done = False
         reward_for_game = []
@@ -62,7 +62,7 @@ def plots(dqn):
 
 
 def test_parameters():
-    budget = [8192 << exponent for exponent in range(8)]  # [8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
+    budget = [8192 << exponent for exponent in range(6)]  # [8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
     gamma = [0.99, 0.9]
     alpha = [1, 0.5]
     wuf = [64 << exponent for exponent in range(5)]  # [64, 128, 256, 512, 1024]
@@ -70,37 +70,27 @@ def test_parameters():
     return list(itertools.product(*[budget, gamma, alpha, wuf]))
 
 
-
-
 if __name__ == '__main__':
-    n_per_model = 1
+    n_per_model = 4
     n_tests = 100
     settings = test_parameters()
     print(len(settings))
     for parameter in settings:
         model_name = '_'.join([str(x) for x in parameter])
-#--------------------------------------------------------------------------------------------------------------------
-        try:
-            for i in range(n_per_model):
-                model_name_n = model_name + '_V' + str("1")
-#====================================================================================================================
-                if os.path.isdir(join('output', model_name_n)) == False:
-                    model_place = join('output', model_name_n, 'model1_' + str(parameter[0]) + '.h5')
-                    dqn = DQN('MountainCar-v0', model_name_n, SettingsHere(
-                        parameter[0],
-                        parameter[1],
-                        parameter[2],
-                        parameter[3],
-                    ))
-                    dqn.train(False)
-                    dqn.plots(sum)
-                    test_model(model_place, n_tests, model_name_n)
-                    plots(dqn)
-                else:
-                    with open('log.txt', 'a') as myfile:
-                        myfile.write(model_name_n + " already exists" + '\n')
-# ====================================================================================================================
-        except PermissionError:
-            with open('error_log.txt', 'a') as myfile:
-                myfile.write(model_name_n + "\tPermission Error" + '\n')
-#--------------------------------------------------------------------------------------------------------------------
+        for i in range(n_per_model):
+            model_name_n = model_name + '_V' + str(i + 1)
+            if os.path.isdir(join('output', model_name_n)) == False:
+                model_place = join('output', model_name_n, 'model1_' + str(parameter[0]) + '.h5')
+                dqn = DQN('MountainCar-v0', model_name_n, SettingsHere(
+                    parameter[0],
+                    parameter[1],
+                    parameter[2],
+                    parameter[3],
+                ))
+                dqn.train(False)
+                dqn.plots(sum)
+                test_model(model_place, n_tests, model_name_n)
+                plots(dqn)
+            else:
+                with open('log.txt', 'a') as myfile:
+                    myfile.write(model_name_n + " already exists" + '\n')
